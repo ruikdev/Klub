7e9bbe91643f02e4cb7223a8b98239b334b8239d
Axel <166296471+ruikdev@users.noreply.github.com>
2026-02-18 10:34:18 +0100

    feat: ajouter le composant 'cours' avec la logique de navigation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DevoirsComponent } from './devoirs/devoirs.component';
 import { NotesComponent } from './notes/notes.component';
+import { CoursComponent } from './cours/cours.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, DevoirsComponent, NotesComponent],
+  imports: [CommonModule, DevoirsComponent, NotesComponent, CoursComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -17,7 +18,7 @@ export class AppComponent {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  currentPage = 'devoirs';
+  currentPage = 'cours';
 
   togglePage(page: string) {
       this.currentPage = page.toLowerCase();
