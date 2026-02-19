9a55c5a3530fd8d90d419f00cfc46a373a3986f8
Axel <166296471+ruikdev@users.noreply.github.com>
2026-02-19 10:43:35 +0100

    feat: ajouter le composant OCR avec HTML, CSS

---
+++ frontend/src/app/ocr/ocr.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ocr',
+  standalone: true,
+  imports: [],
+  templateUrl: './ocr.component.html',
+  styleUrl: './ocr.component.css'
+})
+export class OcrComponent {
+
+}
