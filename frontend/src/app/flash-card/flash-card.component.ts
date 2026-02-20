8d1a542af1af8697465192ed96f8d3106e73dcbd
Axel <166296471+ruikdev@users.noreply.github.com>
2026-02-20 09:34:08 +0100

    feat: ajouter le composant FlashCard avec son template et ses styles

---
+++ frontend/src/app/flash-card/flash-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flash-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './flash-card.component.html',
+  styleUrl: './flash-card.component.css'
+})
+export class FlashCardComponent {
+
+}
